51e1f1c4b4b4972e1e4f4728bb2d998666f67487
gsanjay1708 <30703377+gsanjay1708@users.noreply.github.com>
2024-04-27 20:08:44 +0530

    Added_Hello_World

--- app/src/androidTest/java/com/example/sharemybill/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/sharemybill/ExampleInstrumentedTest.kt
@@ -20,5 +20,6 @@ class ExampleInstrumentedTest {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
         assertEquals("com.example.sharemybill", appContext.packageName)
+        print("Hello World!!!")
     }
 }
